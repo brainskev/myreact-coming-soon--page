a47f1f23b4574f9cc494764dec0da615e880dfd0
kelvin <ngotho.kelvin@yahoo.com>
2024-03-04 14:09:16 +0300

    Add a vercel config

---
+++ build/precache-manifest.713c4c0973dd8a80a9c79c51dbd27c90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39167dc802e30621913bd3dae9ac8100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92248a7c43792e5b2348",
+    "url": "/static/css/main.d41fed98.chunk.css"
+  },
+  {
+    "revision": "255aa3bb7dcbc8d8e4ee",
+    "url": "/static/js/2.37c8826f.chunk.js"
+  },
+  {
+    "revision": "92248a7c43792e5b2348",
+    "url": "/static/js/main.3f95fb08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
